bfa9e4806ca131e8165c00c79b8b2d048775b135
sik2 <bickwang123@gmail.com>
2025-01-17 15:24:46 +0900

    work

--- src/main/java/com/ll/k8s/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/k8s/domain/home/home/controller/HomeController.java
@@ -19,7 +19,7 @@ public class HomeController {
         InetAddress localHost = InetAddress.getLocalHost();
         // getHostName 메소드를 호출하여 호스트 이름을 얻습니다.
         String hostname = localHost.getHostName();
-        return "home, hostname : " + hostname;
+        return "home!!!, hostname : " + hostname;
     }
 
     @GetMapping("/jwtSecretKey")
