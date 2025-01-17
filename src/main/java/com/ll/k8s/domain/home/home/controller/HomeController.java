fd57ae59ff41fd8151a7e84f44433180b41c96ff
sik2 <bickwang123@gmail.com>
2025-01-17 16:13:39 +0900

    deploy

--- src/main/java/com/ll/k8s/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/k8s/domain/home/home/controller/HomeController.java
@@ -19,7 +19,7 @@ public class HomeController {
         InetAddress localHost = InetAddress.getLocalHost();
         // getHostName 메소드를 호출하여 호스트 이름을 얻습니다.
         String hostname = localHost.getHostName();
-        return "home ci/cd deploy!!!!, hostname is : " + hostname;
+        return "home ci/cd deploy!!!!!, hostname is : " + hostname;
     }
 
     @GetMapping("/jwtSecretKey")
