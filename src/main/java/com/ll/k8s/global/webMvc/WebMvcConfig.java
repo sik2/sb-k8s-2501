c7d581afb768309ea256401370bf2aaed5b77319
sik2 <bickwang123@gmail.com>
2025-01-17 21:37:14 +0900

    set efs

---
+++ src/main/java/com/ll/k8s/global/webMvc/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package com.ll.k8s.global.webMvc;
+
+import com.ll.k8s.global.AppConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/gen/**")
+                .addResourceLocations("file:///" + AppConfig.getGenDirPath() + "/");
+    }
+}
